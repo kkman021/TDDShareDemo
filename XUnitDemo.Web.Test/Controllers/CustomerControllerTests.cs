b8d94759b6c093ee465b66278efa34bdc580a2c9
楊明勳 Ming <ming@miniasp.com>
2016-03-21 15:55:55 +0800

    以我出發的網頁測試程式：FluentAutomation

--- XUnitDemo.Web.Test/Controllers/CustomerControllerTests.cs
+++ XUnitDemo.Web.Test/Controllers/CustomerControllerTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Web.Mvc;
 using FluentAssertions;
+using FluentAutomation;
 using NSubstitute;
 using NSubstitute.Core;
 using OpenQA.Selenium;
@@ -15,37 +16,29 @@ using XunitDemo.Web.Controllers;
 
 namespace XUnitDemo.Web.Test.Controllers
 {
-    public class CustomerControllerTests : IDisposable
+    public class CustomerControllerTests : FluentTest, IDisposable
     {
-        private IWebDriver driver;
         private string baseURL;
 
         public CustomerControllerTests()
         {
-            driver = new ChromeDriver();
+            SeleniumWebDriver.Bootstrap(SeleniumWebDriver.Browser.Chrome);
             baseURL = "http://localhost:39765/";
         }
 
+        [Fact]
         public void Index_Login_Selenium_Fail()
         {
-            driver.Navigate().GoToUrl(baseURL + "/Home/Login");
-            driver.FindElement(By.Id("Account")).Clear();
-            driver.FindElement(By.Id("Account")).SendKeys("test");
-            driver.FindElement(By.Id("Pwd")).Clear();
-            driver.FindElement(By.Id("Pwd")).SendKeys("245");
-            driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
-
             var expectMsg = "帳號密碼異常";
-            var acturlMsg = driver
-                            .FindElement(By.CssSelector("div.validation-summary-errors > ul > li"))
-                            .Text;
 
-            expectMsg.Should().Be(acturlMsg);
-        }
+            I.Open(baseURL + "/Home/Login");
 
-        public void Dispose()
-        {
-            driver.Quit();
+            I.Enter("test").In("#Account")
+             .Enter("245").In("#Pwd");
+
+            I.Click("input[type=\"submit\"]");
+
+            I.Assert.True(() => I.Find(".validation-summary-errors").Element.Text.Contains(expectMsg));
         }
     }
 }
