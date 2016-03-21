47b15a720e4142cbeb9a7dc26f3fa091237a5109
楊明勳 Ming <ming@miniasp.com>
2016-03-21 14:36:17 +0800

    Nsubstitute – 判斷相依物件互動

---
+++ Presentation/XunitDemo.Web/Controllers/CustomerController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using XunitDemo.Entity;
+using XunitDemo.Services;
+
+namespace XunitDemo.Web.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private ICustomerService _customerService;
+
+        public CustomerController(ICustomerService customerService)
+        {
+            this._customerService = customerService;
+        }
+
+        public ActionResult Index(string city)
+        {
+            List<Customer> model = new List<Customer>();
+            if (string.IsNullOrWhiteSpace(city))
+            {
+                ViewBag.ErrorMsg = "請輸入居住地";
+            }
+            else
+            {
+                model = _customerService.GetCustomers(city);
+            }
+            return View(model);
+        }
+    }
+}
---
+++ XUnitDemo.Web.Test/Controllers/CustomerControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using XunitDemo.Web.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using XunitDemo.Services;
+using NSubstitute;
+
+namespace XunitDemo.Web.Controllers.Tests
+{
+    public class CustomerControllerTests
+    {
+
+        [Fact()]
+        public void IndexTest()
+        {
+            //arrange
+            var city = "";
+            var customerService = Substitute.For<ICustomerService>();
+
+            //act
+            var target = new CustomerController(customerService);
+            target.Index(city);
+
+            //assert
+            customerService.Received(1).GetCustomers(city);
+        }
+    }
+}
